af605787c9ff0a02e51a37d7f4af48cf7ea80231
Ricardo-shuo-liu <13838152117@139.com>
2025-11-14 21:59:25 +0800

    哈哈哈，我是刘硕。没想到扒出你的GitHub了，学王海洋留一个痕迹
    小小pr奉上，能不能给我合并

---
+++ This is a trace.py
@@ -0,0 +1,35 @@
+def function()->None:
+  printf("hello zhifan")
+
+if __name__ == "__main__":
+  function()
+
+"""
+using System;
+
+namespace hello
+{
+  public class myclass
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("hello zhifan");
+    }
+    
+  }
+    
+}
+"""
+
+"""
+#include<stdio.h>
+void function()
+{
+  printf("hello zhifan");
+}
+int main()
+{
+  function();
+  return 0;
+}
+"""
